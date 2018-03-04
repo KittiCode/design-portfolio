f1d94e6e64c81c82f487aae1e560e596b4b24244
Kittichote Chain <chain_chainna@hotmail.com>
2018-03-04 16:44:48 +0000

    showcase done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function openCity(evt, project) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(project).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openNav(evt, project) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("navcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("navlinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(project).style.display = "block";
+    evt.currentTarget.className += " active";
+}
